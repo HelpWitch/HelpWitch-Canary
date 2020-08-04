443f014bbab5dec5c66980545bcaa9834f4f1c12
JI <tierbestdev@gmail.com>
2020-08-04 12:48:32 +0300

    part II

--- main.js
+++ main.js
@@ -11,7 +11,8 @@ function createWindow () {
 
   // Create the browser window.
   win = new BrowserWindow({width: 1024, height: 768})
-
+  win.show()
+  win.setMenuBarVisibility(false)
   // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
